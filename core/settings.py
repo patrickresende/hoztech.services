702baed6fef85c919e8999614678857c9f27ba26
Patrick Resende <108430576+patrickresende@users.noreply.github.com>
2025-06-01 17:40:16 -0300

    testando deploy 5

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com',
+    'hoz-tech.onrender.com',
+]
 
 # Application definition
 INSTALLED_APPS = [
