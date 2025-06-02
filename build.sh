3e56cec88ddd32aedf1354dbd1505120a6c8f0b7
Patrick Resende <108430576+patrickresende@users.noreply.github.com>
2025-06-02 15:50:21 -0300

    agora vai 20.1

--- build.sh
+++ build.sh
@@ -6,8 +6,28 @@ echo "Installing dependencies..."
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "Checking static files integrity..."
+if [ -f "static/images/logo.png" ]; then
+    echo "Logo file exists"
+    file static/images/logo.png
+    ls -l static/images/logo.png
+else
+    echo "ERROR: Logo file not found!"
+    exit 1
+fi
+
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Verifying collected files..."
+if [ -f "staticfiles/images/logo.png" ]; then
+    echo "Collected logo file exists"
+    file staticfiles/images/logo.png
+    ls -l staticfiles/images/logo.png
+else
+    echo "ERROR: Collected logo file not found!"
+    exit 1
+fi
 
 echo "Running migrations..."
 python manage.py migrate
--- mysite/settings.py
+++ mysite/settings.py
@@ -18,13 +18,22 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 # Application definition
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Third party apps
     'whitenoise.runserver_nostatic',
+    'django_browser_reload',
+    'compressor',
+    
+    # Local apps
+    'core.apps.CoreConfig',
+    'theme.apps.ThemeConfig',
 ]
 
 MIDDLEWARE = [
@@ -36,10 +45,28 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Database configuration
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
@@ -50,7 +77,60 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# WhiteNoise Configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_ALLOW_ALL_ORIGINS = True
+WHITENOISE_MAX_AGE = 31536000
+WHITENOISE_AUTOREFRESH = True
+WHITENOISE_MIMETYPES = {
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.ico': 'image/x-icon',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+}
+
+# Django Compressor settings
+COMPRESS_ENABLED = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.rCSSMinFilter',
+]
+COMPRESS_JS_FILTERS = [
+    'compressor.filters.jsmin.JSMinFilter',
+]
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+# Logging Configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+        'whitenoise': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
 
 # Media files
 MEDIA_URL = '/media/'
